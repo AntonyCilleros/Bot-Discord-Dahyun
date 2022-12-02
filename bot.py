673ab60f07a4393a12de2a310d8dfbe087f49308
antony cilleros <antonycilleros9@gmail.com>
2022-12-02 03:39:15 +0100

    Ajout de la commande spam

--- bot.py
+++ bot.py
@@ -57,6 +57,10 @@ def run_discord_bot_message():
 
 	client.run(TOKEN)
 
+
+
+
+
 def run_discord_bot_command():
 	load_dotenv()
 	TOKEN = os.getenv('DISCORD_TOKEN')
@@ -66,6 +70,14 @@ def run_discord_bot_command():
 	intents.members = True
 	bot = commands.Bot(command_prefix="!", intents=intents)
 
+	@bot.command(name='spam', help='Spam l''utilisateur spécifié. \nSyntaxe: !spam <utilisateur> <nombre de messages> <message>')
+	@has_permissions(administrator=True)
+	async def spam(ctx, user:discord.Member, amount=10, *, message="Je suis Dahyun"):
+		print("spamming " + str(user) + " with " + message)
+		embed = discord.Embed(title=message, color=0xff0000)
+		for i in range(amount):
+			await user.send(embed=embed)
+			time.sleep(0.5)
 	
 	@bot.command(name='clear', help='Efface les x derniers messages. Sinon, efface le dernier message')
 	@has_permissions(manage_messages=True)
@@ -83,7 +95,7 @@ def run_discord_bot_command():
 		users = []
 		for guild in bot.guilds:
 			for member in guild.members:
-				if ((str)(member).find("Dahyun#6588") == -1): 
+				if ((str)(member).find("Dahyun#6588") == -1):
 					users.append(member)
 		await ctx.channel.send(random.choice(users))
 
@@ -111,42 +123,14 @@ def run_discord_bot_command():
 
 	bot.run(TOKEN)
 
-""" load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
-intents = discord.Intents.default()
-intents.members = True
 
-client = discord.Client()
-bot = commands.Bot(command_prefix="!", intents=intents)
 
-@bot.command(name='quatuor',help='Choisit un des membres du quatuor au hasard')
-async def quatuor(ctx):
-	quatuor = ['Antony','Alex','Romain','Maxime']
-	await ctx.send(random.choice(quatuor))
 
-@bot.command(name='goulag',help='Choisit un membre au hasard')
-async def goulag(ctx):
-	users = []
-	for guild in bot.guilds:
-		for member in guild.members:
-			if ((str)(member).find("Dahyun#6588") == -1): 
-				users.append(member)
-	await ctx.send(random.choice(users))
 
-@client.event
-async def on_message(message):
-	if 'happy birthday' in message.content.lower() or 'joyeux an' in message.content.lower() or 'bon an' in message.content.lower():
-		await message.channel.send('Joyeux anniversaire ! 🎈🎉')
 
-@bot.command(name='clear', help='Efface les x derniers messages. Sinon, efface le dernier message')
-@has_permissions(manage_messages=True)
-async def clear(ctx, amount=1):
-	await ctx.channel.purge(limit=amount+1)
 
-#Les vraies fonctions
-
+"""
 def check_port(host, port):
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
         if sock.connect_ex((host, port)) == 0:
@@ -154,72 +138,35 @@ def check_port(host, port):
         else:
             return False
 
-# @bot.command(name='terra-restart', help='Démarre ou redémarre les serveurs Terraria')
-# @commands.has_any_role('hyper', 475077706837655564, 'admin', 'terraria')
-# async def restartTerraServers(ctx):
-# 	if ctx.channel.name == 'terraria' :
-# 		start = time.time()
-# 		await ctx.send('Ok, c\'est en cours')
-# 		os.system(f'./terraria/runTerrariaServers.sh ENTER')
-# 		while True:
-# 			time.sleep(1)
-# 			if check_port('127.0.0.1',7777) and check_port('127.0.0.1',8888):
-# 				break
-# 		await ctx.send('Serveurs Terraria ouverts en ' + str(round(time.time()-start)) + ' secondes')
+@bot.command(name='terra-restart', help='Démarre ou redémarre les serveurs Terraria')
+@commands.has_any_role('hyper', 475077706837655564, 'admin', 'terraria')
+async def restartTerraServers(ctx):
+	if ctx.channel.name == 'terraria' :
+		start = time.time()
+		await ctx.send('Ok, c\'est en cours')
+		os.system(f'./terraria/runTerrariaServers.sh ENTER')
+		while True:
+			time.sleep(1)
+			if check_port('127.0.0.1',7777) and check_port('127.0.0.1',8888):
+				break
+		await ctx.send('Serveurs Terraria ouverts en ' + str(round(time.time()-start)) + ' secondes')
 
-# @bot.command(name='terra-close', help='Ferme les serveurs Terraria')
-# @commands.has_any_role('hyper', 475077706837655564, 'admin', 'terraria')
-# async def closeTerraServers(ctx):
-# 	if ctx.channel.name == 'terraria' :
-# 		await ctx.send('Attends je sauvegarde')
-# 		os.system(f'./terraria/closeTerrariaServers.sh ENTER')
-# 		await ctx.send('C\'est bon')
+@bot.command(name='terra-close', help='Ferme les serveurs Terraria')
+@commands.has_any_role('hyper', 475077706837655564, 'admin', 'terraria')
+async def closeTerraServers(ctx):
+	if ctx.channel.name == 'terraria' :
+		await ctx.send('Attends je sauvegarde')
+		os.system(f'./terraria/closeTerrariaServers.sh ENTER')
+		await ctx.send('C\'est bon')
 
-# @bot.command(name='terra-stats', help='Retourne les statistiques des serveurs Terraria')
-# async def statsTerraServers(ctx):
-# 	if ctx.channel.name == 'terraria' :
-# 		main = ':no_entry:'
-# 		sec = ':no_entry:'
-# 		if check_port('127.0.0.1', 7777):
-# 			main=':white_check_mark:'
-# 		if check_port('127.0.0.1', 8888):
-# 			sec=':white_check_mark:'
-# 		await ctx.send(main + '  Serveur principale\n' + sec + '  Serveur secondaire ')
-
-domainWhiteListed = []
-@bot.command(name='dns-add-w', help="Ajoute un domaine sur la liste blanche\nA utiliser pour débloquer un domaine qui semble bloqué par erreur\nVous pouvez ajouter plusieurs domaines en les séparant d'un espace")
-async def dnsAddW(ctx, *, domain):
-	os.system('''ssh root@192.168.42.63 "pihole -w ''' + domain + '''"''')
-	domainWhiteListed.append(domain)
-	await ctx.send('Domaine(s)' + domain + ' ajouté(s) à la liste blanche')
-
-@bot.command(name='dns-cancel-w', help="Annule la dernière ittération de dns-add-w")
-async def dnsDelW(ctx):
-	if len(domainWhiteListed) > 0:
-		os.system('''ssh root@192.168.42.63 "pihole -w -d ''' + domainWhiteListed[-1] + '''"''')
-		domainWhiteListed.pop()
-		await ctx.send('Domaine(s) retiré(s) de la liste blanche')
-	else:
-		await ctx.send('Aucun domaine n\'a été ajouté dernièrement')
-
-@client.event
-async def on_reaction_add(reaction, user):
-    if user != client.user:
-        if str(reaction.emoji) == "➡️":
-            #fetch new results from the Spotify API
-            newSearchResult = discord.Embed(...)
-            await reaction.message.edit(embed=newSearchResult)
-        if str(reaction.emoji) == "⬅️":
-            #fetch new results from the Spotify API
-            newSearchResult = discord.Embed(...)
-            await reaction.message.edit(embed=newSearchResult)
-
-@bot.event
-async def on_command_error(ctx, error):
-	response = ['T\'as cru t\'étais qui ?', 'Mais pour qui te prends-tu ?', 'Calme toi', 'Calme toi misérable', 'Redescends sale INSOLENT !']
-	if isinstance(error, commands.errors.CheckFailure):
-		await ctx.send(random.choice(response))
-
-
-bot.run(TOKEN)
+@bot.command(name='terra-stats', help='Retourne les statistiques des serveurs Terraria')
+async def statsTerraServers(ctx):
+	if ctx.channel.name == 'terraria' :
+		main = ':no_entry:'
+		sec = ':no_entry:'
+		if check_port('127.0.0.1', 7777):
+			main=':white_check_mark:'
+		if check_port('127.0.0.1', 8888):
+			sec=':white_check_mark:'
+		await ctx.send(main + '  Serveur principale\n' + sec + '  Serveur secondaire ')
 """
